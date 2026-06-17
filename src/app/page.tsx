24bb572a89c6c807a3f7b2b38fb3c982a781532b
chisator <gianfrancosacchi@hotmail.com.ar>
2026-06-16 21:19:47 -0300

    fix: Usar img nativo para imagenes de categorias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -233,12 +233,11 @@ export default function HomePage() {
             >
               {cat.image ? (
                 <div className="absolute inset-0">
-                  <Image
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
                     src={cat.image}
                     alt={cat.name}
-                    fill
-                    className="object-cover transition-transform duration-700 group-hover:scale-[1.03]"
-                    sizes="(max-width: 768px) 100vw, 33vw"
+                    className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-[1.03]"
                   />
                   <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent" />
                 </div>
